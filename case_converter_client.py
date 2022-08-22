46ba262aeb55eaeb628cb7d8bdcfd6033fd9fc19
BernardoBeling <berbeling@gmail.com>
2022-08-22 11:52:41 -0300

    Adding client and server

---
+++ case_converter_client.py
@@ -0,0 +1,45 @@
+from socket import *
+
+server = 'localhost'  
+port = 9999
+keep = 1
+
+conn_type = int(input('Connection type:\n0 - TCP (default)\n1 - UDP\n-> ') or 0) #0: TCP, 1: UDP
+if conn_type == 0:
+   with socket(AF_INET,SOCK_STREAM)	as client: # creates an tcp socket (client side)
+    client.connect((server,port))
+
+    while keep != 0:
+        txt = input('Enter your sentence: ')
+        to = int(input('Convertion type:\nTo lower: 0\nTo upper (default): 1\n-> ') or 1)
+
+        msg = '1;' + txt if to else '0;' + txt
+
+        enc_msg = msg.encode()
+        client.sendto(enc_msg, (server, port)) 
+
+        mod_msg, serverIP = client.recvfrom(1500)         
+        dec_msg = mod_msg.decode()
+
+        print('-'*5+f'\n{dec_msg}\n'+'-'*5)   # print received/decoded message
+
+        keep = 0 if input('\nWant to enter another sentence?\nYes: Any key, No: 0\n-> ') == '0' else 1
+
+
+elif conn_type == 1:
+    clientSocket = socket(AF_INET, SOCK_DGRAM)  # creates a socket (client side)
+
+    txt = input('Enter your sentence: ')
+    to = int(input('Convertion type:\nTo lower: 0\nTo upper (default): 1\n-> ') or 1)
+
+    msg = '1;' + txt if to else '0;' + txt
+
+    encodedMessage = msg.encode()
+    clientSocket.sendto(encodedMessage, (server, port))     # sends the message to server (no need to use connect() before, since it is UDP)
+
+    modifiedMessage, serverIP = clientSocket.recvfrom(1500)         # 1500 bytes are read from the UDP socket
+    decodedMessage = modifiedMessage.decode()
+
+    print(decodedMessage)   # print received/decoded message
+
+    clientSocket.close()
---
+++ case_converter_server.py
@@ -0,0 +1,46 @@
+from base64 import decode
+from socket import *
+
+host = 'localhost'  
+port = 9999	
+
+conn_type = int(input('Connection type:\n0 - TCP (default)\n1 - UDP\n-> ') or 0) #0: TCP, 1: UDP
+
+if conn_type == 0:
+   with socket(AF_INET,SOCK_STREAM)	as server: # creates an tcp socket (server side)    
+        server.bind((host,port))
+        server.listen()
+        print(f'Server is listening on port {port} with TCP connection!')
+        conn, addr = server.accept()
+        
+        with conn:
+            print(f'Connection succesfull with client {addr}')
+            while 1:
+                msg = conn.recv(1500)
+                if not msg:
+                    break
+                dec_msg = msg.decode()
+                print(f'>Received message: {msg}\n>Decoded messsage: {dec_msg}')
+                
+                to, txt = dec_msg.split(';')
+
+                new_txt = txt.upper() if int(to) else txt.lower() #0: to lower  1: to upper
+                enc_msg = new_txt.encode()
+                print(f'>Modified message: {new_txt}\n>Encoded messsage: {enc_msg}')
+
+                conn.send(enc_msg)
+else:
+    server = socket(AF_INET,SOCK_DGRAM)	# creates an udp socket (server side)
+    server.bind((host, port))	# bind() associates the socket with its local address [bind() is used in the server side]
+    print(f'Server is listening on port {port} with UDP connection!')
+
+    while 1:
+        message, clientIP = server.recvfrom(1500)		# 1500 bytes are read from the UDP socket
+        decodedMessage = message.decode()
+        print(f'Received message: {message}, Decoded messsage: {decodedMessage}')
+
+        modifiedMessage = decodedMessage.upper()
+        encodedMessage = modifiedMessage.encode()
+        print(f'Modified message: {modifiedMessage}, Encoded messsage: {encodedMessage}')
+
+        server.sendto(encodedMessage, clientIP)		# sends converted (upper-case) sentence
