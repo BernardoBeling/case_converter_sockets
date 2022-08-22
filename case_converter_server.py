78533e8fe875e5a126b3b152feca0dfa73a4be2e
BernardoBeling <berbeling@gmail.com>
2022-08-22 19:33:39 -0300

    Fixing TCP connections removing python's with command

--- case_converter_server.py
+++ case_converter_server.py
@@ -8,28 +8,28 @@ port = 9999
 conn_type = int(input('Connection type:\n0 - TCP (default)\n1 - UDP\n-> ') or 0) #0: TCP, 1: UDP
 
 if conn_type == 0:
-   with socket(AF_INET,SOCK_STREAM)	as server: # creates an tcp socket (server side)    
-        server.bind((host,port))
-        server.listen()
-        print(f'Server is listening on {host}:{port} with TCP connection!')
-        
-        while 1:
-            conn, addr = server.accept()
-            with conn:
-                print(f'Connection succesfull with client {addr}')
-                msg = conn.recv(1500)
-                if not msg:
-                    break
-                dec_msg = msg.decode()
-                print(f'>Received message: {msg}\n>Decoded messsage: {dec_msg}')
-                
-                to, txt = dec_msg.split(';')
+    server = socket(AF_INET,SOCK_STREAM)	as server: # creates an tcp socket (server side)    
+    server.bind((host,port))
+    server.listen()
+    print(f'Server is listening on {host}:{port} with TCP connection!')
 
-                new_txt = txt.upper() if int(to) else txt.lower() #0: to lower  1: to upper
-                enc_msg = new_txt.encode()
-                print(f'>Modified message: {new_txt}\n>Encoded messsage: {enc_msg}')
+    while 1:
+        conn, addr = server.accept()
+        with conn:
+            print(f'Connection succesfull with client {addr}')
+            msg = conn.recv(1500)
+            if not msg:
+                break
+            dec_msg = msg.decode()
+            print(f'>Received message: {msg}\n>Decoded messsage: {dec_msg}')
+            
+            to, txt = dec_msg.split(';')
 
-                conn.send(enc_msg)
+            new_txt = txt.upper() if int(to) else txt.lower() #0: to lower  1: to upper
+            enc_msg = new_txt.encode()
+            print(f'>Modified message: {new_txt}\n>Encoded messsage: {enc_msg}')
+
+            conn.send(enc_msg)
 else:
     server = socket(AF_INET,SOCK_DGRAM)	# creates an udp socket (server side)
     server.bind((host, port))	# bind() associates the socket with its local address [bind() is used in the server side]
